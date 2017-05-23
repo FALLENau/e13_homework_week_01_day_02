fd49adea6ac400c0a203975cb49550ef9a3adf5d
FALLENau <reece.c.jones@gmail.com>
2017-05-23 17:40:29 +0100

    test 9

--- start_point/ruby_functions_practice.rb
+++ start_point/ruby_functions_practice.rb
@@ -32,3 +32,21 @@ end
 def add_string_as_number(string_1, string_2)
   return string_1.to_i + string_2.to_i
 end
+
+def number_to_full_month_name(number)
+  if number == 1
+    return "January"
+  elsif number == 3
+    return "March"
+  elsif number == 9
+    return "September"
+  end
+
+  return results
+  # first_month_string = number_to_full_month_name( 1 )
+  # third_month_string = number_to_full_month_name( 3 )
+  # ninth_month_string = number_to_full_month_name( 9 )
+  # assert_equal( "January", first_month_string )
+  # assert_equal( "March", third_month_string )
+  # assert_equal( "September", ninth_month_string )
+end
